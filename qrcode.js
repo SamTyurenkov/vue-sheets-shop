505cbfc2a9b42125defdefd88e4aec9f029a747d
Sam Tyurenkov <sam.tyurenkov@gmail.com>
2025-08-11 21:13:19 +0300

    change social

---
+++ qrcode.js
@@ -0,0 +1,24 @@
+// Import the qrcode package
+const QRCode = require('qrcode');
+
+// Define your website URL
+const websiteURL = 'https://polyana-sale.web.app';
+
+// Generate the QR code as a string (for terminal output)
+QRCode.toString(websiteURL, { type: 'terminal' }, (err, qrCodeString) => {
+  if (err) {
+    console.error('Error generating QR code:', err);
+    return;
+  }
+  console.log('QR Code for your website:');
+  console.log(qrCodeString);
+});
+
+// Generate the QR code as an image file (e.g., PNG)
+QRCode.toFile('./qrcode.png', websiteURL, { errorCorrectionLevel: 'H' }, (err) => {
+  if (err) {
+    console.error('Error saving QR code image:', err);
+    return;
+  }
+  console.log('QR code saved as qrcode.png');
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -79,9 +79,9 @@ async function fetchImagesForItem(itemIndex, driveFolderUrl) {
     if (images.length > 0 && images[0] && images[0].id) {
       await fetchImageAsBlob(images[0].id)
     }
-    
+
     // Other images will be loaded when they become active in the swiper
-    
+
     // Ensure DOM is updated before continuing
     await nextTick()
   } catch (error) {
@@ -164,7 +164,8 @@ onMounted(async () => {
     <h1 class="text-2xl font-bold mb-4">Продаю в Красной Поляне</h1>
 
     <div class="grid gap-6 overflow-hidden">
-      <div v-for="(item, idx) in items" :key="idx" class="border-t flex flex-wrap gap-y-4 pt-6 max-w-full overflow-hidden">
+      <div v-for="(item, idx) in items" :key="idx"
+        class="border-t flex flex-wrap gap-y-4 pt-6 max-w-full overflow-hidden">
 
         <div class="basis-full sm:basis-1/2 order-2 sm:order-1 sm:pe-4">
           <!-- Item Details -->
@@ -188,8 +189,8 @@ onMounted(async () => {
 
           <!-- Images -->
           <ImageSwiper v-else-if="itemImages[idx] && itemImages[idx].length > 0" class="w-full min-w-0"
-            :key="`swiper-${idx}-${itemImages[idx].length}`"
-            :images="itemImages[idx]" :loading-images="loadingImages" @open-lightbox="handleSwiperLightbox" />
+            :key="`swiper-${idx}-${itemImages[idx].length}`" :images="itemImages[idx]" :loading-images="loadingImages"
+            @open-lightbox="handleSwiperLightbox" />
 
 
         </div>
@@ -204,26 +205,24 @@ onMounted(async () => {
     <LightboxGallery :is-open="lightboxOpen" :images="lightboxImages" :initial-index="lightboxInitialIndex"
       @close="closeLightbox" />
 
-    <div class="fixed pointer-events-none bottom-3 start-0 w-full z-10">
-      <div class="max-w-4xl w-[100vw] mx-auto flex gap-3 *:block justify-end pe-3">
-        <a class="bg-[#158a2e] pointer-events-auto rounded-full *:fill-white p-[6px] hover:bg-[#2dab48]"
-          href="https://wa.me/+79819190599" rel="noindex nofollow noopener noreferrer" target="_blank">
-          <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-            <path
-              d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z">
-            </path>
-          </svg>
-        </a>
-        <a class="bg-[#0088cc] pointer-events-auto rounded-full *:fill-white p-[6px] hover:bg-[#3daae0]"
-          href="https://t.me/sam_tyurenkov" rel="noindex nofollow noopener noreferrer" target="_blank">
-          <svg class="relative -start-[1px]" width="30" height="30" xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512">
-            <path
-              d="M446.7 98.6l-67.6 318.8c-5.1 22.5-18.4 28.1-37.3 17.5l-103-75.9-49.7 47.8c-5.5 5.5-10.1 10.1-20.7 10.1l7.4-104.9 190.9-172.5c8.3-7.4-1.8-11.5-12.9-4.1L117.8 284 16.2 252.2c-22.1-6.9-22.5-22.1 4.6-32.7L418.2 66.4c18.4-6.9 34.5 4.1 28.5 32.2z">
-            </path>
-          </svg>
-        </a>
-      </div>
+    <div class="fixed pointer-events-none top-3 end-1 z-10">
+      <a class="bg-[#158a2e] block pointer-events-auto rounded-full *:fill-white p-[6px] hover:bg-[#2dab48] mb-4 border-2 border-white"
+        href="https://wa.me/+79819190599" rel="noindex nofollow noopener noreferrer" target="_blank">
+        <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+          <path
+            d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z">
+          </path>
+        </svg>
+      </a>
+      <a class="bg-[#0088cc] block pointer-events-auto rounded-full *:fill-white p-[6px] hover:bg-[#3daae0] border-2 border-white"
+        href="https://t.me/sam_tyurenkov" rel="noindex nofollow noopener noreferrer" target="_blank">
+        <svg class="relative -start-[1px]" width="30" height="30" xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512">
+          <path
+            d="M446.7 98.6l-67.6 318.8c-5.1 22.5-18.4 28.1-37.3 17.5l-103-75.9-49.7 47.8c-5.5 5.5-10.1 10.1-20.7 10.1l7.4-104.9 190.9-172.5c8.3-7.4-1.8-11.5-12.9-4.1L117.8 284 16.2 252.2c-22.1-6.9-22.5-22.1 4.6-32.7L418.2 66.4c18.4-6.9 34.5 4.1 28.5 32.2z">
+          </path>
+        </svg>
+      </a>
     </div>
 
   </div>
